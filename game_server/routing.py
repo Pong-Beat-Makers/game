0ba5150a1d269ca112fa56b51fc6be61ffcdd37c
sunwooiljoon <wnsdl1659@naver.com>
2024-03-08 17:43:45 +0900

    feat : waiting room consumer routing 추가
    random, tournament 추가
    그룹화 수정

--- game_server/routing.py
+++ game_server/routing.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from channels.routing import URLRouter
+from django.urls import path, include
 
 from game_server import consumers
 
+sub_url_patterns = [
+    path("<uuid:room_id>/", consumers.GameServerConsumer.as_asgi()),
+    path("waitingroom/random/", consumers.RandomWaitingRoomConsumer.as_asgi()),
+    path("waitingroom/tournament/", consumers.TournamentWaitingRoomConsumer.as_asgi()),
+]
+
 websocket_urlpatterns = [
-    path("ws/game/<uuid:room_id>/", consumers.GameServerConsumer.as_asgi()),
+    path("ws/game/", URLRouter(sub_url_patterns)),
 ]
