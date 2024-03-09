f194f215cac2c1201531c0d151357975da0da9c9
sunwooiljoon <wnsdl1659@naver.com>
2024-03-09 20:41:05 +0900

    chore: Docker 설정 변경
    - migration 할 app 추가
    - PORT 기본 8000 설정 및 수정 가능

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-python manage.py makemigrations game_data
-python manage.py migrate game_data
+python manage.py makemigrations game_data game_server
+python manage.py migrate game_data game_server
 
-daphne -b 0.0.0.0 -p 8000 config.asgi:application
+daphne -b 0.0.0.0 -p $PORT config.asgi:application
