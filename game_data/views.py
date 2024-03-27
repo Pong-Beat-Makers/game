f6f77529b854d95491ce3fe4e94b4e0806d3bf62
sunwooiljoon <wnsdl1659@naver.com>
2024-03-27 19:08:00 +0900

    fix : game data 조회 관련 로직 수정
    - 존재하지 않는 id인 경우 404 반환
    - id는 존재하지만 게임 데이터가 없을 경우 빈 배열반환

--- game_data/views.py
+++ game_data/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db.models import Q
 from rest_framework import status
 from rest_framework.response import Response
@@ -7,6 +9,7 @@ from .models import GameDataModel
 from config.verifying_user import verifying_user
 from config.utils import get_token
 from rest_framework.exceptions import AuthenticationFailed
+import requests
 
 # Create your views here.
 
@@ -15,12 +18,16 @@ class GameDataListView(APIView):
         try:
             verifying_user(get_token(request))
             id = request.GET['id']
+            url = f'{os.environ.get("USER_MANAGEMENT_SERVER")}api/user-management/profile/?id={id}'
+            res = requests.get(url, headers={"Authorization": f"Bearer {get_token(request)}"})
+            if res.status_code == 404:
+                return Response({'error': 'id not found'}, status=status.HTTP_404_NOT_FOUND)
+
+
             game_data_queryset = GameDataModel.objects.filter(
                 Q(user1_id=id) | Q(user2_id=id)
             ).order_by('-created_at')
 
-            if not game_data_queryset.exists():
-                return Response({'error': 'data not found'}, status=status.HTTP_404_NOT_FOUND)
 
             serializer = GameDataSerializer(game_data_queryset, many=True,
                                             context={'id': id})
