3dc9d4965d95a4becc77f18d2742ab54c8072119
sunwooiljoon <wnsdl1659@naver.com>
2024-03-29 14:09:38 +0900

    fix : ALLOWED_HOSTS 수정
    - 기본 localhost 설정
    - 추후 직접 설정 가능

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
-    'localhost',
+    os.environ.get('DOMAIN', 'localhost'),
 ]
 
 
